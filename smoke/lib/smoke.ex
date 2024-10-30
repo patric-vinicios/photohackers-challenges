7ec29c95fc3df8dba5d8cc2062f6e5694d57c3a3
Patric Vinicios <psdev7@gmail.com>
2024-10-29 21:42:14 -0300

    feat: create smoke test project

---
+++ smoke/lib/smoke.ex
@@ -0,0 +1,18 @@
+defmodule Smoke do
+  @moduledoc """
+  Documentation for `Smoke`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Smoke.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ smoke/lib/smoke/application.ex
@@ -0,0 +1,20 @@
+defmodule Smoke.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Smoke.Worker.start_link(arg)
+      # {Smoke.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Smoke.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
